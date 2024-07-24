0535e8b3443f65bfe53d0c0a9ce45cbd5075c307
Sadique Ahmed <140674441+Sadiqueahmed@users.noreply.github.com>
2024-07-24 11:01:15 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,20 @@
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active');
+    }else{
+        document.querySelector('.header').classList.remove('active');
+    }
+};
+
+
+var swiper = new Swiper(".home-slider", {
+    autoplay: {
+    delay: 5000,
+    },
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
